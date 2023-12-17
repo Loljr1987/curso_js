4a6e888d3b32d014947c1fc1e0a081eb8e146bf3
Pequeno <lucivaldojr@hotmail.com>
2023-12-17 12:51:05 -0300

    continuando...

--- primeiro-programa.js
+++ primeiro-programa.js
@@ -1,1 +1,10 @@
-console.log("Meu Primeiro Programa");
+console.log("Meu Primeiro Programa: trabalhando com Variavéis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+10);
+
+const idadeSomada = idade+10;
+
+console.log(idadeSomada)
